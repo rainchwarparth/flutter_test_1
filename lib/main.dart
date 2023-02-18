98bd929f843c9749c058f2daa4472813fae03e16
parth.rainchwar <parth.rainchwar@profect.hk>
2023-02-19 00:44:23 +0530

    commit 1#

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const Center(
+        child: Text(
+      'Testing if this thing works but with a concern that hot relaod is not working properly',
+      textDirection: TextDirection.rtl,
+    ))
+  );
+  }
+
